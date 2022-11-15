c2fdbfef7ac6ae3c367b1ed99ed20a081cfffb81
Simon Fugl <sifug20@student.sdu.dk>
2022-11-15 23:05:46 +0100

    Copy the same file to path for faster testing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import Redbubble
 import Upscaler
+import shutil
 from Upscaler import *
 from Redbubble import *
 import os
+
 
 # assign directory
 
@@ -11,14 +13,17 @@ import os
 
 
 def main():
-    directory = 'resources'
+    # os.remove('resources/Black Hole #space #ai #blackhole.png')
+
+    shutil.copyfile('Black Hole #space #ai #blackhole.png', 'resources/Black Hole #space #ai #blackhole.png')
+    # os.popen("copy 'Black Hole #space #ai #blackhole.png' './resources/Black Hole #space #ai #blackhole.png'")
+    directory = './resources'
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         # checking if it is a file
         if os.path.isfile(f):
-            Upscaler.upScaleImage(f)
+            Upscaler.upScaleImage('resources\Black Hole #space #ai #blackhole.png')
             Redbubble.uploadToRedbubble()
-
 
 
 if __name__ == "__main__":
